548ba0304b66826dbae084cf50db04d71a07b635
Yein E <yeinneg@gmail.com>
2023-07-26 00:23:27 -0300

    corrigiendo error de envio email verificacion

--- backend/utils/mail.config.js
+++ backend/utils/mail.config.js
@@ -2,11 +2,6 @@ const nodemailer = require('nodemailer');
 require("dotenv").config();
 const { google } = require('googleapis');
 
-/*const CLIENT_ID = "261837536175-2gt7j7gghbs3fi6mf9htao0sn91lvrpm.apps.googleusercontent.com";
-const CLIENT_SECRET = "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";
-const REDIRECT_URI = "https://developers.google.com/oauthplayground";
-const REFRESH_TOKEN = "1//04g86At207WnNCgYIARAAGAQSNwF-L9IrhIEPWib6mJxdxC3pFoqvKwQxX-VURVCGun-d1q9eyraRnr8mzmwn4DqxhOfk-TzXU0Y";*/
-
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
@@ -16,7 +11,7 @@ const oAuth2client = new google.auth.OAuth2(
   CLIENT_ID,
   CLIENT_SECRET,
   REDIRECT_URI,
-);
+); 
 
 oAuth2client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
@@ -35,7 +30,7 @@ async function sendVerificationEmail(user, registerToken) {
         refreshToken: REFRESH_TOKEN,
         accessToken: accessToken,
       },
-    });   
+    });  
     
     const mailOptions = {
         from: "pocketpal.nocountry@gmail.com", // Reemplaza con tu dirección de Gmail
@@ -45,17 +40,17 @@ async function sendVerificationEmail(user, registerToken) {
         <a href="https://ibb.co/hgMML36"><img src="https://i.ibb.co/6yZZrjM/Imagen-Email.png" alt="Imagen-Email" border="0"></a>,
         <p>Hola ${user.firstName},        
         </p><p>Por favor, haz clic en el siguiente enlace para verificar tu correo electrónico:</p>
-        <p><a href="http://localhost:1237/api/users/confirm/${registerToken}">Verificar Correo</a></p>`, 
+        <p><a href="https://wallet-project-nocountry-backend-production-y.up.railway.app/api/users/confirm/${registerToken}">Verificar Correo</a></p>`, 
         
       };
     console.log("Enviando correo de verificación..."); // para debuguear
-    console.log(registerToken+"a");
+    console.log(registerToken);
 
    result = await transporter.sendMail(mailOptions);  
 
     
-  } 
-  
+  } ;
+
    // Enviar correo de con clave temporal
 
 async function sendrecoveryPasswordEmail(existingUser, recovPassword) {
@@ -92,7 +87,8 @@ async function sendrecoveryPasswordEmail(existingUser, recovPassword) {
 } 
  
 
-  module.exports = {
+  
+ module.exports = {
     sendVerificationEmail,
     sendrecoveryPasswordEmail
   }
